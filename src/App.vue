6c8b5e21974522210286464153c4231e594bc7b5
Eden <gresseden@gmail.com>
2022-01-27 12:55:16 -0500

    working on wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,147 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mobile-breakpoint="768"
+      app
+    >
+    <v-img
+        class="pa-4 pt-7"
+        height="190"
+        src="img/space.jpg"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      >
+
+      <v-avatar
+          size="70"
+          class="mb-2"
+        >
+          <img
+            v-if="userDB"
+            :src="userDB.image ? userDB.image : defaultImage "
+            alt="User Avatar"
+          >
+          <img
+            v-if="!userDB"
+            :src="defaultImage"
+            alt="User Avatar"
+          >
+        </v-avatar>
+        <!-- <div v-if="userDB">
+          <div class="white--text text-subtitle-1 font-weight-bold">{{ userDB.name ? userDB.name : ''}}</div>
+          <div class="white--text text-subtitle-2">{{ userDB.email ? userDB.email : ''}}</div>
+        </div>
+        <div v-if="!userDB">
+          <div class="white--text text-subtitle-1 font-weight-bold"></div>
+          <div class="white--text text-subtitle-2"></div>
+        </div> -->
+      </v-img>
+
+      <v-list
+        
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+  
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#primary"
+      dark
+      prominent
+      src="img/space.jpg"
+      :height="$route.path === '/todo' ? 220 : 190 "
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.9), rgba(128,208,199,.9)"
+        ></v-img>
+      </template>
+
+      <v-container class="header-container pa-0">
+
+        
+        <v-row>
+
+          <v-app-bar-nav-icon 
+            
+            @click="drawer = !drawer"
+          ></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <!-- <search v-if="$route.path === '/todo'" /> -->
+          <!-- <rigth-side-toolbar /> -->
+        </v-row>
+
+
+        <v-row>
+          <v-app-bar-title class="ml-4 text-h4">
+            {{ title }}
+          </v-app-bar-title>
+        </v-row>
+        <v-row id="row__tools">
+          <!-- <live-date-time /> -->
+          <!-- <local-weather /> -->
+        </v-row>
+      
+        <!-- <v-row v-if="$route.path === '/todo' ">
+          <field-add-task />
+        </v-row> -->
+
+      </v-container>
+
+    </v-app-bar>
+
+    <!-- <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar> -->
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+      defaultImage: 'https://lenguajejs.com/javascript/logo.svg',
+      title: 'Inspection',
+      items: [
+      { title: 'Todo', icon: 'mdi-format-list-checks', to: '/todo' },
+      { title: 'About', icon: 'mdi-help-box', to: '/about' },
+      { title: 'Profile', icon: 'mdi-emoticon-cool-outline', to: '/profile' },
+      { title: 'Stripe API', icon: 'mdi-cart', to: '/stripe' },
+    ],
+  }),
   }
-}
+</script>
+
+<style lang="sass">
+.header-container
+  max-width: none !important
+#row__tools
+  display: flex
+  justify-content: space-between
+  align-items: center
+  margin-right: 20px
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// import { initializeApp } from "firebase/app";
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMQqWCRuR1CpIsdm-93gHG5IJi_yhg9k8",
+  authDomain: "inspection-6c319.firebaseapp.com",
+  projectId: "inspection-6c319",
+  storageBucket: "inspection-6c319.appspot.com",
+  messagingSenderId: "300530642294",
+  appId: "1:300530642294:web:6540c3d91d5f8775cc9e57"
+};
+
+// Initialize Firebase
+//   
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const time = firebase.firestore.FieldValue.serverTimeStamp
+
+export { auth, db, time };
