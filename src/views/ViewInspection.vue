b36831c97b4422c9e16133dbce4df0bab1f1cb07
Eden <gresseden@gmail.com>
2022-02-19 18:08:56 -0500

    Still fixing routes /view-inspection

---
+++ src/views/ViewInspection.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid> 
+    <v-row class="text-center">
+      <v-col cols="12" sm="12" justify-center align-center class="info">
+        <h1>View Inspection</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
