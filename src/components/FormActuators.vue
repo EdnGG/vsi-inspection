6b0a877f15b4f6d67f21b8258393cf4123d2e90c
Eden <gresseden@gmail.com>
2022-03-01 21:06:09 -0500

    Still working on data flow

--- src/components/FormActuators.vue
+++ src/components/FormActuators.vue
@@ -1,136 +1,175 @@
 <template>
   <v-container fluid mt-5 pa-5>
-        <!--  -->
-        <v-form  @submit.prevent="addActuator" ref="form">
-          <v-container v-if="!firstVal">
-          <!-- <v-container> -->
-          <v-row class="d-flex justify-space-around" >
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model.trim="inspection.inspection.number"
-                :rules="inspectionRules"
-                :counter="10"
-                label="Inspection #"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model.trim="inspection.inspection.date"
-                :rules="inspectionRules"
-                :counter="10"
-                label="Date"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model.trim="inspection.inspection.technical"
-                :rules="inspectionRules"
-                label="Technical Name"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-col>
-            <v-row>
-              <v-container>
-            <v-row>
-              <v-col>
-                <v-btn 
-                  @click="validateFirstSection"
-                  block
-                  elevation="2"
-                >
-                  Next
-                </v-btn> 
-              </v-col>
-            </v-row>
-          </v-container>
-            </v-row>
+    <!--  -->
+    <v-form @submit.prevent="addActuator" ref="form">
+      <v-container v-if="!firstVal">
+        <!-- <v-container> -->
+        <v-row class="d-flex justify-space-around">
+          <v-col cols="12" md="3">
+            <v-text-field
+              v-model.trim="inspection.inspectionInfo.id"
+              :rules="inspectionRules"
+              :counter="10"
+              label="Inspection #"
+              required
+            ></v-text-field>
           </v-col>
-          </v-container>
-          <!-- Button -->
-          <v-container v-if="firstVal">
-          <!-- <v-container> -->
-          <v-row class="d-flex justify-space-around">
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model.trim="inspection.actuator"
-                :rules="inspectionRules"
-                label="Actuator Model"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model.trim="inspection.controlPack"
-                :rules="inspectionRules"
-                label="Control Pack Model"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
+          <v-col cols="12" md="3">
+            <v-text-field
+              v-model.trim="inspection.inspectionInfo.date"
+              :rules="inspectionRules"
+              :counter="10"
+              label="Date"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-text-field
+              v-model.trim="inspection.inspectionInfo.technical"
+              :rules="inspectionRules"
+              label="Technical Name"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-col>
           <v-row>
-            <v-col cols="12">
-              <v-container fluid>               
-                <!--  -->
-                <custom-field v-model="inspection.visual" title="Visual"/>
-                <custom-field v-model="inspection.waterInspection" title="Water Inspection"/>
-                <custom-field v-model="inspection.operationalTest" title="Operational Test"/>
-                <custom-field v-model="inspection.wireCompartiment" title="Wire Compartiment"/>
-                <custom-field v-model="inspection.handwheelBoltPatern" title="Handwheel Bolt Patern"/> 
-                <!--  -->
-                <p>Array con todos los objetos: {{totalInspection}}</p>
-                <p>Objeto actuator  : {{inspection}}</p>
-              </v-container>
-            </v-col>
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-btn @click="validateFirstSection" block elevation="2">
+                    Next
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
           </v-row>
-          <v-row>
-            <v-col cols="12">
-              <v-container fluid>
-                <v-textarea
-                  clearable
-                  clear-icon="mdi-close-circle"
-                  label="Notes:"
-                  v-model.trim="inspection.observaciones"
-                ></v-textarea>
-              </v-container>
-            </v-col>
-          </v-row>
-          Datos array:  {{ datosArray}}
-          <v-container class="button-container">
-          <v-row align-center justify-space-around>
-            <v-col  class="button-container" cols="12" xs="6" text-xs-center>
-              <!-- <v-btn 
-                class="ma-5 col-xs-6" 
-                xs="6" color="warning" 
-                :disabled="!prevButton"
-              > Prev Actuator 
-              </v-btn> -->
-              <v-btn 
-                class="ma-5 col-xs-6" 
-                xs="6" color="warning" 
+        </v-col>
+      </v-container>
+      <!-- Button -->
+      <v-container v-if="firstVal">
+        <!-- <v-container> -->
+        <v-row class="d-flex justify-space-around">
+          <v-col cols="12" md="3">
+            <v-text-field
+              v-model.trim="inspection.inspectionInfo.data.actuator"
+              :rules="inspectionRules"
+              label="Actuator Model"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-text-field
+              v-model.trim="inspection.inspectionInfo.data.controlPack"
+              :rules="inspectionRules"
+              label="Control Pack Model"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-container fluid>
+              <!--  -->
+              <custom-field
+                v-model="inspection.inspectionInfo.data.visual"
+                title="Visual"
+              />
+              <custom-field
+                v-model="inspection.inspectionInfo.data.waterInspection"
+                title="Water Inspection"
+              />
+              <custom-field
+                v-model="inspection.inspectionInfo.data.operationalTest"
+                title="Operational Test"
+              />
+              <custom-field
+                v-model="inspection.inspectionInfo.data.wireCompartiment"
+                title="Wire Compartiment"
+              />
+              <custom-field
+                v-model="inspection.inspectionInfo.data.handwheelBoltPatern"
+                title="Handwheel Bolt Patern"
+              />
+              <!--  -->
+              <p>Array con todos los objetos: {{ totalInspection }}</p>
+              <p>Objeto actuator : {{ inspection }}</p>
+            </v-container>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-container fluid>
+              <v-textarea
+                clearable
+                clear-icon="mdi-close-circle"
+                label="Notes:"
+                v-model.trim="inspection.inspectionInfo.data.observaciones"
+              ></v-textarea>
+            </v-container>
+          </v-col>
+        </v-row>
+        Datos array: {{ datosArray }}
+        <v-container class="button-container mobile-container">
+          <v-row align-center justify-center>
+            <v-col class="" cols="12" xs="6" text-xs-center>
+              <v-btn
+                class="ma-5 col-xs-6"
+                xs="6"
+                color="warning"
                 :disabled="nextButton"
                 @click="addActuator"
-              > Next Actuator </v-btn>
+              >
+                Add Actuator
+              </v-btn>
+              <!--  -->
+              <!-- <v-col class="button-container" cols="12" xs="6" text-xs-center> -->
+              <v-dialog 
+                style="padding-top: 10px;"
+                v-model="dialog" 
+                persistent 
+                max-width="500"       
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn 
+                    color="primary" 
+                    dark 
+                    v-bind="attrs" 
+                    v-on="on"
+                    class="ma-5 col-xs-6"
+                    xs="6"
+                  >
+                      End inspection
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title class="text-h5">
+                    Are you sure sending inspection?
+                  </v-card-title>
+                  <v-card-text
+                    >
+                    Sending the inspection will be post on the DB and no change can be done.
+                  </v-card-text
+                  >
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" text @click="dialog = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn color="green darken-1" text @click="submit">
+                      Submit Inspection
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            <!-- </v-col> -->
+              <!--  -->
             </v-col>
           </v-row>
-          </v-container>
-          <v-col cols="12">
-              <v-btn
-                color="success"
-                type="submit"
-                block
-                @click="submit"
-              >
-                Submit Inspection
-              </v-btn>
-            </v-col>
-          <v-container>
-          </v-container>
-          </v-container>
-        </v-form>
+        </v-container>
+        <v-container> </v-container>
+      </v-container>
+    </v-form>
   </v-container>
 </template>
 
@@ -142,55 +181,46 @@ export default {
     "custom-field": require("@/components/CustomField.vue").default,
   },
   data: () => ({
+    dialog: false,
     firstVal: false,
     prevButton: false,
     nextButton: false,
-    // value1: "",
-    // value2: "",
-    // value3: "",
-    // value4: "",
-    // value5: "",
-    // Checar aqui
     select: { state: "", item: "Select one" },
     totalInspection: [],
     // initial object
+    // ESTE SERIA EL MODELO DE DATOS
     inspection: {
-      inspection: {
-        number: "",
+      inspectionInfo: {
+        id: "",
         date: "",
         technical: "",
+        data: {
+          actuator: "",
+          controlPack: "",
+          visual: "",
+          waterInspection: "",
+          operationalTest: "",
+          wireCompartiment: "",
+          handwheelBoltPatern: "",
+          observaciones: "",
+        },
       },
-      // value1: "",
-      // value2: "",
-      // value3: "",
-      // value4: "",
-      // value5: "",
-      actuator: "",
-      observaciones: "",
-      controlPack: "",
-      visual: "",
-      waterInspection: "",
-      operationalTest: "",
-      wireCompartiment: "",
-      handwheelBoltPatern: "",
     },
-    inspectionRules: [
-      (v) => !!v || "Field is required",
-      // length
-      // (v) => v.length <= 10 || "Field must be less than 10 characters",
-    ],
+    inspectionRules: [(v) => !!v || "Field is required"],
   }),
   computed: {
     ...mapState(["inspections", "allInspections"]),
     datosArray() {
       return [
-        this.inspection.visual,
-        this.inspection.waterInspection,
-        this.inspection.operationalTest,
-        this.inspection.wireCompartiment,
-        this.inspection.handwheelBoltPatern,
+        this.inspection.inspectionInfo.data.visual,
+        this.inspection.inspectionInfo.data.waterInspection,
+        this.inspection.inspectionInfo.data.operationalTest,
+        this.inspection.inspectionInfo.data.wireCompartiment,
+        this.inspection.inspectionInfo.data.handwheelBoltPatern,
       ].filter((item) => {
-        if (item) return item;
+        if (item) {
+          return item;
+        }
       });
     },
     hide() {
@@ -203,12 +233,11 @@ export default {
     ...mapActions(["addInspection", "addActuator"]),
     validateFirstSection() {
       if (
-        this.inspection.inspection.number &&
-        this.inspection.inspection.date &&
-        this.inspection.inspection.technical
+        this.inspection.inspectionInfo.id &&
+        this.inspection.inspectionInfo.date &&
+        this.inspection.inspectionInfo.technical
       ) {
         return (this.firstVal = true);
-        // display: this.firstVal ? "none" : "block",
       }
       return (this.firstVal = false);
     },
@@ -216,41 +245,53 @@ export default {
       this.totalInspection.push(this.inspection);
       this.$store.dispatch("addActuator", this.inspection);
       console.log("all inspections: ", this.totalInspection);
-      // this.$refs.form.reset();
-      // this.inspection = {
-      // inspection: {
-      //   number = this.totalInspection.inspection.inspection.number,
-      //   date =  this.totalInspection.inspection.inspection.number,
-      //   technical = this.totalInspection.inspection.inspection.number,
-      // },
-      // inspection: {
-      //   number: "",
-      //   date: "",
-      //   technical: "",
-      // },
-      // actuator: "",
-      // controlPack: "",
-      // observaciones: "",
-      // visual: "",
-      // waterInspection: "",
-      // operationalTest: "",
-      // wireCompartiment: "",
-      // handwheelBoltPatern: "",
-      // };
+
+      // ESTE SERIA EL MODELO DE DATOS
+      this.inspection = {
+        // OBJETO QUE SE MANDARA A FIRESTORE
+        inspectionInfo: {
+          // OBJETO QUE SE GENERA POR CADA MOTOR
+          //id: "", // valor por default
+          //date: "",  // valor por default
+          //technical: "",  // valor por default
+          data: {
+            actuator: "", // valor a variar
+            controlPack: "", // valor a variar
+            visual: "", // valor a variar
+            waterInspection: "", // valor a variar
+            operationalTest: "", // valor a variar
+            wireCompartiment: "", // valor a variar
+            handwheelBoltPatern: "", // valor a variar
+            observaciones: "", // valor a variar
+          },
+        },
+      };
     },
     submit() {
+      this.dialog = false;
       this.$store.dispatch("addInspection", this.totalInspection);
       console.log("Enviando inspeccion a firebase");
       this.$refs.form.reset();
+      this.firstVal = false;
     },
   },
 };
 </script>
 
 <style scoped>
+@media (max-width: 390px) {
+  .mobile-container {
+    display: flex;
+    /* flex-wrap: wrap; */
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+
 .button-container {
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-around;
-  width: 70%;
+  /* width: 0%; */
 }
 </style>
