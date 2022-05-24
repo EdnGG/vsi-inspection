11094317f661fb8562c439e585b4f2363c9e18bb
Eden <gresseden@gmail.com>
2022-05-24 17:21:42 -0400

    doing some progress

--- src/components/inspection/form/Actuators.vue
+++ src/components/inspection/form/Actuators.vue
@@ -1,6 +1,152 @@
 <template>
 <v-container>
+
 <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step
+        :complete="e1 > 1"
+        step="1"
+      >
+        Step 1
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step
+        :complete="e1 > 2"
+        step="2"
+      >
+        Step 2
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="3">
+        Step 3
+      </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        >
+        <v-form @submit.prevent="addActuator" ref="form">
+         <v-row class="d-flex justify-space-around">
+        <v-col cols="12" md="3">
+          <v-text-field
+            v-model.trim="inspection.inspectionInfo.id"
+            :rules="inspectionRules"
+            :counter="10"
+            label="Inspection #"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-menu
+            ref="menuDate"
+            v-model="menuDate"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="inspection.inspectionInfo.date"
+                label="Date"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="inspection.inspectionInfo.date"
+              no-title
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="menuDate = false">
+                Cancel
+              </v-btn>
+              <v-btn text color="primary" @click="$refs.menuDate.save(date)">
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            v-model.trim="inspection.inspectionInfo.technical"
+            :rules="inspectionRules"
+            label="Technical Name"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      </v-form>
+        
+        
+        
+        
+        
+        </v-card>
+        <v-btn
+          color="primary"
+          @click="e1 = 2"
+        >
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
+
+        <v-btn
+          color="primary"
+          @click="e1 = 3"
+        >
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
+
+        <v-btn
+          color="primary"
+          @click="e1 = 1"
+        >
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+<!-- <v-stepper v-model="e1">
         <v-stepper-header>
           <v-stepper-step :complete="e1 > 1" step="1">
             Step 1
@@ -18,11 +164,10 @@
         </v-stepper-header>
   <v-form @submit.prevent="addActuator" ref="form">
     <v-container>
-      <!-- <v-container v-show="!firstVal"> -->
       
  <v-stepper-items>
       <v-stepper-content step="1">
-       <!-- First content -->
+       First content
         <v-row class="d-flex justify-space-around">
         <v-col cols="12" md="3">
           <v-text-field
@@ -78,7 +223,7 @@
         </v-col>
       </v-row>
     </v-container>
-<!-- Finish first content -->
+Finish first content
         <v-btn
           color="primary"
           @click="e1 = 2"
@@ -92,7 +237,7 @@
       </v-stepper-content>
 
       <v-stepper-content step="2">
-        <!-- Second content -->
+        Second content
          <v-container>
       <v-row class="d-flex justify-space-around">
         <v-col cols="12" md="3">
@@ -204,7 +349,7 @@
         </v-row>
       </v-container>
     </v-container>
-        <!-- Finished second content -->
+        Finished second content
 
         <v-btn
           color="primary"
@@ -239,197 +384,9 @@
       </v-stepper-content>
     </v-stepper-items>
 </v-form>
-  </v-stepper>
-
-
+</v-stepper>
+</v-container> -->
 </v-container>
-<!--  -->
-      
-
-      <!--  -->
-      <!-- <v-row class="d-flex justify-space-around">
-        <v-col cols="12" md="3">
-          <v-text-field
-            v-model.trim="inspection.inspectionInfo.id"
-            :rules="inspectionRules"
-            :counter="10"
-            label="Inspection #"
-            required
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" md="3">
-          <v-menu
-            ref="menuDate"
-            v-model="menuDate"
-            :close-on-content-click="false"
-            :return-value.sync="date"
-            transition="scale-transition"
-            offset-y
-            min-width="auto"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="inspection.inspectionInfo.date"
-                label="Date"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="inspection.inspectionInfo.date"
-              no-title
-              scrollable
-            >
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="menuDate = false">
-                Cancel
-              </v-btn>
-              <v-btn text color="primary" @click="$refs.menuDate.save(date)">
-                OK
-              </v-btn>
-            </v-date-picker>
-          </v-menu>
-        </v-col>
-        <v-col cols="12" md="3">
-          <v-text-field
-            v-model.trim="inspection.inspectionInfo.technical"
-            :rules="inspectionRules"
-            label="Technical Name"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row> -->
-      <!-- <v-col>
-        <v-row>
-          <v-container>
-            <v-row>
-              <v-col>
-                <v-btn @click="validateFirstSection" block elevation="2">
-                  Next
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-row>
-      </v-col> -->
-    <!-- </v-container> -->
-    <!-- Button -->
-    <!-- <v-container v-show="firstVal"> -->
-    <!-- <v-container>
-      <v-row class="d-flex justify-space-around">
-        <v-col cols="12" md="3">
-          <v-text-field
-            v-model="inspection.inspectionInfo.data.actuator"
-            label="Actuator Model"
-            required
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" md="3">
-          <v-text-field
-            v-model="inspection.inspectionInfo.data.controlPack"
-            label="Control Pack Model"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-container fluid>
-            <core-custom-select
-              v-model="inspection.inspectionInfo.data.visual"
-              title="Visual"
-            ></core-custom-select>
-            <core-custom-select
-              v-model="inspection.inspectionInfo.data.waterInspection"
-              title="Water Inspection"
-            ></core-custom-select>
-            <core-custom-select
-              v-model="inspection.inspectionInfo.data.operationalTest"
-              title="Operational Test"
-            ></core-custom-select>
-            <core-custom-select
-              v-model="inspection.inspectionInfo.data.wireCompartiment"
-              title="Wire Compartiment"
-            ></core-custom-select>
-            <core-custom-select
-              v-model="inspection.inspectionInfo.data.handwheelBoltPatern"
-              title="Handwheel Bolt Patern"
-            ></core-custom-select>
-            <p>Array con todos los objetos: {{ totalInspection }}</p>
-            <p>Objeto actuator : {{ inspection }}</p>
-          </v-container>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-container fluid>
-            <v-textarea
-              clearable
-              clear-icon="mdi-close-circle"
-              label="Notes:"
-              v-model.trim="inspection.inspectionInfo.data.observaciones"
-            ></v-textarea>
-          </v-container>
-        </v-col>
-      </v-row>
-      Datos array: {{ datosArray }}
-      <v-container class="button-container mobile-container">
-        <v-row align-center justify-center>
-          <v-col class="" cols="12" xs="6" text-xs-center>
-            <v-btn
-              class="ma-5 col-xs-6"
-              xs="6"
-              color="warning"
-              :disabled="nextButton"
-              @click="addActuator"
-            >
-              Add Actuator
-            </v-btn>
-            <v-dialog
-              style="padding-top: 10px"
-              v-model="dialog"
-              persistent
-              max-width="500"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="primary"
-                  dark
-                  v-bind="attrs"
-                  v-on="on"
-                  class="ma-5 col-xs-6"
-                  xs="6"
-                >
-                  End inspection
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title class="text-h5">
-                  Are you sure sending inspection?
-                </v-card-title>
-                <v-card-text>
-                  Sending the inspection will be post on the DB and no change
-                  can be done.
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green darken-1" text @click="dialog = false">
-                    Cancel
-                  </v-btn>
-                  <v-btn color="green darken-1" text @click="submit">
-                    Submit Inspection
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-container> -->
-  <!-- </v-form>
-  </v-stepper> -->
 </template>
 
 <script>
